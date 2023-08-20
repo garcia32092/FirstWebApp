5a57314dcc0e9473bdd7a38a94c3d8a6cb2decb4
garcia32092 <pops32092@gmail.com>
2023-08-20 02:56:02 -0700

    Improve Home Page

--- firstwebapp/src/components/Home.js
+++ firstwebapp/src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Typography, Box, Button } from '@mui/material';
+import { Container, Typography, Box, Button, Card } from '@mui/material';
 import { Link } from 'react-router-dom';
 import ScrollTrigger from 'react-scroll-trigger';
 import TriggeredContact from './TriggeredContact';
@@ -11,26 +11,49 @@ const Home = () => {
           const multiplier = 1.7;  // Adjust this value to control the fading speed
           const opacity = Math.max(1.6 - (progress * multiplier), 0);
           document.getElementById('homeContainer').style.opacity = opacity;
+          document.getElementById('infoCard').style.opacity = Math.max((progress * multiplier) - 0.6, 0);
         }}>
-        <Container id="homeContainer" style={{
-          backgroundImage: 'url("/HeaderImage.jpg")',
-          backgroundSize: 'cover',
-          minHeight: '100vh',  // Set a minimum height for the Container
+            <Container id="homeContainer" style={{
+                backgroundImage: 'url("/HeaderImage.jpg")',
+                backgroundSize: 'cover',
+                minHeight: '100vh',  // Set a minimum height for the Container
+            }}>
+            </Container>
+        </ScrollTrigger>
+        <Card style={{
+            backgroundColor: 'rgba(255, 255, 255, 0.7)',
+            padding: '10px',
+            textAlign: 'center',
+            position: 'absolute',
+            top: '18%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            height: '70px'
         }}>
-          <Typography variant="h2" gutterBottom>
-            Spartan Mechanical & Service
-          </Typography>
-          <Typography variant="body1" paragraph>
-            We provide the best heating, ventilation, and air conditioning services...
-          </Typography>
-          <Box sx={{ mt: 4 }}>
-            <Button variant="contained" color="primary" component={Link} to="/services">
-              Learn More
-            </Button>
-          </Box>
-        </Container>
-      </ScrollTrigger>
-      <div style={{ marginTop: '950px' }}>
+            <Typography variant="h2" gutterBottom style={{ color: '#cc0000', textShadow: '2px 1px 0.5px black, 0 0 0em black, 0 0 0em black' }}>
+                Spartan Mechanical & Service
+            </Typography>
+        </Card>
+        <Card id="infoCard" style={{
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            padding: '25px',
+            textAlign: 'center',
+            position: 'absolute',
+            top: '130%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            opacity: 0
+        }}>
+            <Typography variant="body1" paragraph>
+                We provide the best heating, ventilation, and air conditioning services...
+            </Typography>
+            <Box sx={{ mt: 4 }}>
+                <Button variant="contained" color="primary" component={Link} to="/services">
+                    Learn More
+                </Button>
+            </Box>
+        </Card>
+      <div style={{ marginTop: '450px' }}>
         <TriggeredContact />
       </div>
     </div>
