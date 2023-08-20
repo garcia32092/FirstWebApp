f5081f82f592e4bed9b0324b70b13fe5723f5f24
garcia32092 <pops32092@gmail.com>
2023-08-20 02:03:48 -0700

    Add scroll fading to background image

--- firstwebapp/src/components/Home.js
+++ firstwebapp/src/components/Home.js
@@ -1,25 +1,40 @@
 import React from 'react';
 import { Container, Typography, Box, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
+import ScrollTrigger from 'react-scroll-trigger';
 import TriggeredContact from './TriggeredContact';
 
 const Home = () => {
-    return (
-        <Container>
-            <Typography variant="h2" gutterBottom>
-                Spartan Mechanical & Service
-            </Typography>
-            <Typography variant="body1" paragraph>
-                We provide the best heating, ventilation, and air conditioning services. Our team of experts is dedicated to ensuring your comfort and satisfaction. Whether you need a simple repair or a complete system installation, we have the expertise and equipment to get the job done right.
-            </Typography>
-            <Box sx={{ mt: 4 }}>
-                <Button variant="contained" color="primary" component={Link} to="/services">
-                    Learn More
-                </Button>
-            </Box>
-            <TriggeredContact />
+  return (
+    <div>
+        <ScrollTrigger onProgress={({ progress }) => {
+          const multiplier = 1.7;  // Adjust this value to control the fading speed
+          const opacity = Math.max(1.6 - (progress * multiplier), 0);
+          document.getElementById('homeContainer').style.opacity = opacity;
+        }}>
+        <Container id="homeContainer" style={{
+          backgroundImage: 'url("/HeaderImage.jpg")',
+          backgroundSize: 'cover',
+          minHeight: '100vh',  // Set a minimum height for the Container
+        }}>
+          <Typography variant="h2" gutterBottom>
+            Spartan Mechanical & Service
+          </Typography>
+          <Typography variant="body1" paragraph>
+            We provide the best heating, ventilation, and air conditioning services...
+          </Typography>
+          <Box sx={{ mt: 4 }}>
+            <Button variant="contained" color="primary" component={Link} to="/services">
+              Learn More
+            </Button>
+          </Box>
         </Container>
-    );
+      </ScrollTrigger>
+      <div style={{ marginTop: '950px' }}>
+        <TriggeredContact />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- firstwebapp/src/components/TriggeredContact.js
+++ firstwebapp/src/components/TriggeredContact.js
@@ -4,11 +4,11 @@ import ScrollTrigger from 'react-scroll-trigger';
 import Contact from './Contact';
 
 const TriggeredContact = () => {
-  const [visible, setVisible] = useState(false);
+    const [opacity, setOpacity] = useState(1);
 
   return (
-    <ScrollTrigger onEnter={() => setVisible(true)} onExit={() => setVisible(false)}>
-      <Container style={{ opacity: visible ? 1 : 0, transition: 'opacity 1s' }}>
+    <ScrollTrigger onEnter={() => setOpacity(1)} onExit={() => setOpacity(0.1)}>
+      <Container style={{ opacity: opacity, transition: 'opacity 8s' }}>
         <Contact />
       </Container>
     </ScrollTrigger>
