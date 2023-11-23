7d56031bcf1c8a35ce39c4ff65fed43b9cbfda5a
garcia32092 <pops32092@gmail.com>
2023-11-23 03:53:57 -0700

    add icon buttons to nav bar for socials

--- firstwebapp/src/components/NavBar.js
+++ firstwebapp/src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { AppBar, Toolbar, Button } from '@mui/material';
+import { AppBar, Toolbar, Button, IconButton } from '@mui/material';
 import { Link } from 'react-router-dom';
+import FacebookSVG from '../assets/facebook.svg';
 
 const NavBar = () => {
     const linkStyle = {
@@ -11,9 +12,17 @@ const NavBar = () => {
         },
     };
 
+    const socialLinks = [
+        // { icon: <TwitterIcon />, url: 'https://twitter.com/real1eli', color: '#1DA1F2' }, // Twitter blue
+        // { icon: <InstagramIcon />, url: 'https://www.instagram.com/real1elias', color: '#C13584' }, // Instagram gradient (pick one of the colors)
+        // { icon: <YouTubeIcon />, url: 'https://www.youtube.com/@real1elias', color: '#FF0000' }, // YouTube red
+        { icon: <img src={FacebookSVG} alt="Facebook" style={{ width: 42, height: 42 }} />, url: 'https://www.facebook.com/real1elias' },
+        // ... add more links here
+    ];
+
     return (
         <AppBar position="static" color="default" style={{ backgroundColor: 'inherit', boxShadow: 'none' }}>
-            <div style={{ display: 'flex', justifyContent: 'center', marginTop: '15px' }}>
+            <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
                 <Link to="/">
                     <img src={process.env.PUBLIC_URL + "/BlackKnowledgeLogo.png"} alt="Logo" width="200" height="142" /> {/* logo png image ratio is 1 to 0.71 */}
                 </Link>
@@ -29,6 +38,28 @@ const NavBar = () => {
                     Links
                 </Button>
             </Toolbar>
+            <div style={{ justifyContent: 'center', display: 'flex' }}>
+                    {socialLinks.map((link, index) => (
+                        <IconButton
+                        className="social-icon-button"
+                        key={index}
+                        href={link.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        size="small"
+                        sx={{
+                            backgroundColor: '#f0f0f0',
+                            borderRadius: '50%',
+                            margin: '0 5px',
+                            '&:hover': {
+                                backgroundColor: '#cccccc',
+                            },
+                        }}
+                        >
+                            {link.icon}
+                        </IconButton>
+                    ))}
+            </div>
         </AppBar>
     );
 }
